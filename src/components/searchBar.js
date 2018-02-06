c2611068359a09f195821b428fa89888280d9d0b
Shane Nelson <swiftyshane@gmail.com>
2018-02-06 13:08:07 -0500

    Added SearchBar events handleing

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -3,12 +3,13 @@
 
 import React, { Component } from 'react';
 
-class SearchBar extends React.Component
+class SearchBar extends Component
 {
-    render ()
-    {
-        return <input />;
-    }
+  render ()
+  {
+	  return <input onChange={event => console.log(event.target.value)} />;
+	}
+
 }
 
 export default SearchBar;
