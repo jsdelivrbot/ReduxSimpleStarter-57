e69899b0226f74fedb5df28dbacb48a0216dcbe1
Shane Nelson <swiftyshane@gmail.com>
2018-02-07 09:23:14 -0500

    Hadnleing the state f searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,15 +1,26 @@
 // This component is called a functional component. 
-// Its literally a fucntion, like swift ;) 
+
 
 import React, { Component } from 'react';
 
 class SearchBar extends Component
 {
-  render ()
-  {
-	  return <input onChange={event => console.log(event.target.value)} />;
+	constructor(props) 
+	{
+		super(props);
+
+		this.state = { term: ''};
 	}
 
+  render ()
+  {
+	  return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+				Value of the input: {this.state.term}
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
