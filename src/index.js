42c8f8c3722288fd6da03acf7fa1c733af2a0020
Shane Nelson <swiftyshane@gmail.com>
2018-02-07 11:23:16 -0500

    Recieving data from youtube API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchBar';
 
 const API_KEY = "AIzaSyAaKgvqL_uNRlt-9h5y5pV59OlC0lAAcLQ"
 
 // Create a new component
 // Component should create HTML
-
 // Take the components HTML and put it on the page (in the DOM)
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data)
+{
+	console.log(data);
+});
 
 const App = () => 
 {
